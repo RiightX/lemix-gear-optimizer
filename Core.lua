5fbed31f33a8b4a5724d9e9a739cce254d074888
oct8l <git@mail.oct8l.com>
2025-11-09 15:00:59 -0600

    Update version to 0.1.0 and add 'bin/' to .gitignore

--- Core.lua
+++ Core.lua
@@ -2,7 +2,7 @@ local addonName, addon = ...
 
 LemixGearOptimizer = addon
 addon.name = addonName
-addon.version = "1.0.0"
+addon.version = "0.1.0"
 
 local frame = CreateFrame("Frame")
 addon.frame = frame
